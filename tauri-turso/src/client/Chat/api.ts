84fec040508e396365e3d6090df106ce12544bc1
中島浩二 <knaka0209@gmail.com>
2025-10-06 16:56:05 +0900

    up, 1006a9

--- tauri-turso/src/client/Chat/api.ts
+++ tauri-turso/src/client/Chat/api.ts
@@ -54,67 +54,13 @@ export const itemsApi = {
     const resp = await DataUtil.threadlist(post_id, "chat_thread", "desc")
     console.log(resp)
     return resp
-
-    /*
-    const response = await fetch(`/api/chat_thread/list?content=${content}&post_id=${post_id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Failed to create item');
-    }    
-    const json = await response.json();
-    */    
-    //console.log(json);
-
-    /*
-    let dataValue = {};
-    const newItems = [];
-    json.data.forEach((element) => {
-      try{
-        dataValue = JSON.parse(element.data);
-        element.data = dataValue;
-      }catch(e){
-        console.error(e);
-      }
-      newItems.push(element);
-    });    
-    return newItems;
-    */
-   //return [];
-
   },
 
   threadCreate: async (item: NewItem): Promise<Item> => {
     console.log(item);
-    //const target =  JSON.stringify(item);  
-    //console.log(target)
     const ret = await DataUtil.threadCreate(
       "chat_thread", item.data, item.post_id
     )
-
-    /*
-    const send = {
-      content : "chat_thread",
-      post_id: item.post_id,
-      data : item.data 
-    }
-    const response = await fetch("/api/chat_thread/create", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(send),
-    });
-    if (!response.ok) {
-      throw new Error('Failed to create item');
-    }
-
-    return response.json();
-    */
-
   },
 
   threadDelete: async (item: NewItem): Promise<Item> => {
